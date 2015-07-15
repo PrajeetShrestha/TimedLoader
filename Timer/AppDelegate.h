120d2ae171dc22d78965f8b2d6c5561c9b191f77
Prajeet Shrestha <prajeet@ekbana.com>
2015-07-15 16:02:25 +0545

    Initial Commit

---
+++ Timer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Timer
+//
+//  Created by Prajeet Shrestha on 7/15/15.
+//  Copyright (c) 2015 EK Solutions Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Timer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Timer
+//
+//  Created by Prajeet Shrestha on 7/15/15.
+//  Copyright (c) 2015 EK Solutions Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
